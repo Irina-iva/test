bb8deeabc6a098112364b87b9522e9e6f6c98ceb
Irina-iva <irina.ivanova.bis@gmail.com>
2020-12-02 00:17:39 +0400

    Добвалена страница деталей ресурса Добавлен тест проверки деталей ресурса Переименованы тесты

--- src/test/java/tests/googleTests.java
+++ src/test/java/tests/googleTests.java
@@ -1,21 +1,22 @@
 package tests;
 
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import pages.DetailsPage;
 import pages.SearchPage;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class googleTests {
     private static WebDriver driver;
     private static SearchPage searchPage;
+    private static DetailsPage detailsPage;
 
     @BeforeAll
     public static void init(){
@@ -24,17 +25,30 @@ public class googleTests {
         options.addArguments("start-maxinixed");
         driver= new ChromeDriver(options);
         searchPage = new SearchPage(driver);
+        detailsPage = new DetailsPage(driver);
 }
+    @BeforeEach
+public void setup(){
+        driver.get("http://google.com");
+    }
 
     @Test
+    @DisplayName("Поиск числа результатов")
     public void test1() {
-
-    driver.get("http://google.com");
     searchPage.search( "selenium");
     assertEquals(9,searchPage.results.size());
     }
 
-
+    @Test
+    @DisplayName("Проверка деталей ресурса")
+    public void test2 () {
+        searchPage.search("selenium");
+        assertAll(
+                () -> assertEquals("Selenium",detailsPage.getName()),
+                () -> assertEquals("3.141.59 (14 ноября 2018 года)",detailsPage.getLastVersion()),
+                () -> assertEquals("Apache License 2.0",detailsPage.getLicense())
+        );
+    }
     @AfterAll
     public static void teardown() {
         driver.quit();
