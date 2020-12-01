fd697fb6a6b5164645b87a2ea448ada453362596
Irina-iva <irina.ivanova.bis@gmail.com>
2020-12-01 23:19:45 +0400

    Добавлен РОМ Переписан первый тест

--- src/test/java/tests/googleTests.java
+++ src/test/java/tests/googleTests.java
@@ -9,26 +9,32 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import pages.SearchPage;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class googleTests {
     private static WebDriver driver;
+    private static SearchPage searchPage;
+
     @BeforeAll
     public static void init(){
         System.setProperty("webdriver.chromedriver", "src/test/resources/chromedriver.exe");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("start-maxinixed");
         driver= new ChromeDriver(options);
+        searchPage = new SearchPage(driver);
 }
 
     @Test
     public void test1() {
 
     driver.get("http://google.com");
-    driver.findElement(By.cssSelector("input.gLFyf.gsfi")).sendKeys( "selenium", Keys.ENTER);
-    assertEquals(9,driver.findElements(By.cssSelector("#search .g")).size());
+    searchPage.search( "selenium");
+    assertEquals(9,searchPage.results.size());
     }
+
+
     @AfterAll
     public static void teardown() {
         driver.quit();
